eef59131b4a29cca77aa681d19dd6dbed2ac4366
Alex Xu <xulex6@gmail.com>
2023-09-11 22:49:34 +1000

    feat: add custom image block

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -3,64 +3,25 @@ import {
   BlockNoteView,
   lightDefaultTheme,
   useBlockNote,
+  createReactBlockSpec,
+  InlineContent,
+  getDefaultReactSlashMenuItems,
+  ReactSlashMenuItem,
 } from '@blocknote/react';
+import {
+  BlockNoteEditor,
+  BlockSchema,
+  DefaultBlockSchema,
+  defaultBlockSchema,
+  defaultProps,
+} from '@blocknote/core';
 import '@blocknote/core/style.css';
-
-const defaultContent = [
-  {
-    id: 'd78b8f70-f722-4e57-8295-031370ede71d',
-    type: 'heading',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-      level: '1',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Heading',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '66bf374f-ddf9-4507-8ee3-3aaf9a0f6c3d',
-    type: 'heading',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-      level: '2',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Heading 2',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '68b3b0c0-eeab-4cd2-87bb-f24cd71b0070',
-    type: 'heading',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-      level: '3',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Heading 3',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import { fetchAPIRequest } from '../helpers';
+import { Box, TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import ImageUploadBtn from './ImageUploadBtn';
+const placeholder = [
   {
     id: 'f3619d66-662d-4760-bff2-2c0657eefbe9',
     type: 'paragraph',
@@ -78,375 +39,6 @@ const defaultContent = [
     ],
     children: [],
   },
-  {
-    id: '86eaae4b-8f76-4726-b6c5-e6317d70fbde',
-    type: 'bulletListItem',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Bullet list item 1',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '93413c15-16dd-44d1-8c09-e2f32914be88',
-    type: 'bulletListItem',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Bullet list item 2',
-        styles: {},
-      },
-    ],
-    children: [
-      {
-        id: 'e30e1406-78e5-48e7-b94a-ca014d8917de',
-        type: 'bulletListItem',
-        props: {
-          textColor: 'default',
-          backgroundColor: 'default',
-          textAlignment: 'left',
-        },
-        content: [
-          {
-            type: 'text',
-            text: 'Nested bullet tier 1',
-            styles: {},
-          },
-        ],
-        children: [
-          {
-            id: '75597ca6-10de-4acb-bd74-b596d6b067d4',
-            type: 'bulletListItem',
-            props: {
-              textColor: 'default',
-              backgroundColor: 'default',
-              textAlignment: 'left',
-            },
-            content: [
-              {
-                type: 'text',
-                text: 'Nested bullet tier 2',
-                styles: {},
-              },
-            ],
-            children: [],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '1e4d5d0b-cdad-442e-ae87-f57308e1d330',
-    type: 'numberedListItem',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Numbered list item 1',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '1a7d40ff-3498-41cb-b807-5da9f3ed7f7a',
-    type: 'numberedListItem',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Numbered list item 2',
-        styles: {},
-      },
-    ],
-    children: [
-      {
-        id: '1c2c80f5-0416-4c73-95aa-f0d21a11e2a6',
-        type: 'numberedListItem',
-        props: {
-          textColor: 'default',
-          backgroundColor: 'default',
-          textAlignment: 'left',
-        },
-        content: [
-          {
-            type: 'text',
-            text: 'Nested numbered list tier 1',
-            styles: {},
-          },
-        ],
-        children: [
-          {
-            id: 'c17624a6-6945-49ba-ad59-2b1f72cd31a9',
-            type: 'numberedListItem',
-            props: {
-              textColor: 'default',
-              backgroundColor: 'default',
-              textAlignment: 'left',
-            },
-            content: [
-              {
-                type: 'text',
-                text: 'Nested numbered list tier 2',
-                styles: {},
-              },
-            ],
-            children: [],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '3ef9e9f2-f3a2-4fcf-a152-dffcb4accb1d',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Inline text styles → ',
-        styles: {},
-      },
-      {
-        type: 'text',
-        text: 'coloured text',
-        styles: {
-          textColor: 'red',
-        },
-      },
-      {
-        type: 'text',
-        text: '  ',
-        styles: {},
-      },
-      {
-        type: 'text',
-        text: 'italics  ',
-        styles: {
-          italic: true,
-        },
-      },
-      {
-        type: 'text',
-        text: 'bold  ',
-        styles: {
-          bold: true,
-        },
-      },
-      {
-        type: 'text',
-        text: 'underline',
-        styles: {
-          underline: true,
-        },
-      },
-      {
-        type: 'text',
-        text: '  ',
-        styles: {},
-      },
-      {
-        type: 'text',
-        text: 'strikethrough',
-        styles: {
-          strike: true,
-        },
-      },
-      {
-        type: 'text',
-        text: '  ',
-        styles: {
-          bold: true,
-        },
-      },
-    ],
-    children: [],
-  },
-  {
-    id: 'be9d2fe3-1c77-4967-aee5-3a766cd5ee38',
-    type: 'heading',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-      level: '3',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Text alignment',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '1befbd8d-f20d-4c42-acd2-0df38a362719',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Left align (default)',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '07ad2a0b-b65e-4c39-aee2-39928133e933',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'center',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Center align',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: 'f6efe293-0fdc-416e-830b-5e9b818f24e9',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'right',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Right align',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: 'af34c791-8e7b-46ae-be4b-a1eca0450b96',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Raw Link:',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '637306a2-2841-486f-ab78-5400bd2d4a1e',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'link',
-        href: ' https://unsplash.it/500/500',
-        content: [
-          {
-            type: 'text',
-            text: 'https://unsplash.it/500/500',
-            styles: {},
-          },
-        ],
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '1d53fa67-92e2-4e3c-8e80-b4742a4478d5',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'text',
-        text: 'Link with display text:',
-        styles: {},
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '8e3a5f07-d9d7-4e33-bb4b-402269c4fe73',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [
-      {
-        type: 'link',
-        href: ' https://unsplash.it/500/500',
-        content: [
-          {
-            type: 'text',
-            text: 'Random 500 x 500 image from Unsplash API',
-            styles: {},
-          },
-        ],
-      },
-    ],
-    children: [],
-  },
-  {
-    id: '6a8dc313-f848-4b11-a846-827d806170f3',
-    type: 'paragraph',
-    props: {
-      textColor: 'default',
-      backgroundColor: 'default',
-      textAlignment: 'left',
-    },
-    content: [],
-    children: [],
-  },
 ];
 
 // Default dark theme with additional component styles.
@@ -459,20 +51,129 @@ const theme = {
       borderRadius: '10px',
       margin: '0 50px',
       border: `1px solid ${theme.colors.border}`,
-      boxShadow: `0 4px 12px ${theme.colors.shadow}`,
+      // boxShadow: `0 3px 3px ${theme.colors.shadow}`,
       padding: `20px 0px`,
     },
   }),
 };
 
-const Editor = () => {
+const ImageBlockWrapper = createReactBlockSpec({
+  type: 'image',
+  propSchema: {
+    ...defaultProps,
+    src: {
+      default: 'https://via.placeholder.com/1000',
+    },
+    alt: {
+      default: 'image',
+    },
+  },
+  containsInlineContent: false,
+  render: ({ block }) => {
+    return (
+      <div
+        id="image-wrapper"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          src={block.props.src}
+          alt={block.props.alt}
+          contentEditable={false}
+          style={{
+            display: block.props.src ? 'block' : 'none',
+            maxWidth: '100%',
+          }}
+        />
+        <div
+          style={{
+            display: block.props.src ? 'none' : 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <TextField
+            label="Image Embed URL"
+            size="small"
+            onChange={(e) => (block.props.src = e.target.value)}
+          />
+          <div style={{ padding: '0 10px' }}>or</div>
+          <ImageUploadBtn
+            callback={(src) => (block.props.src = src)}
+            compressedCallback={() => {}}
+            btnText={'Upload Image'}
+          />
+        </div>
+        <div style={{ display: block.props.src ? 'none' : 'block' }}>
+          Click to another block to render image.
+        </div>
+      </div>
+    );
+  },
+});
+
+// The custom schema, which includes the default blocks and the custom image
+// block.
+const customSchema = {
+  // Adds all default blocks.
+  ...defaultBlockSchema,
+  // Adds the custom image block.
+  image: ImageBlockWrapper,
+};
+
+// Creates a slash menu item for inserting an image block.
+const insertImage = {
+  name: 'Insert Image',
+  execute: (editor) => {
+    // const src = prompt('Enter image URL');
+    // const alt = prompt("Enter image alt text");
+    const src = '';
+    const alt = 'image';
+    editor.getTextCursorPosition().block.content.text = '---';
+    editor.insertBlocks(
+      [
+        {
+          type: 'image',
+          props: {
+            src: src || '',
+            alt: alt || 'image',
+          },
+        },
+      ],
+      editor.getTextCursorPosition().block,
+      'before'
+    );
+  },
+  aliases: ['image', 'img', 'picture', 'media'],
+  group: 'Media',
+  icon: <ImageOutlinedIcon />,
+  hint: 'Insert an image',
+};
+
+const Editor = ({ initialBlocks, taskId }) => {
+  let timeoutId;
   const editor = useBlockNote({
-    initialContent: defaultContent,
+    initialContent: initialBlocks.length > 0 ? initialBlocks : placeholder,
+    blockSchema: customSchema,
+    slashMenuItems: [
+      ...getDefaultReactSlashMenuItems(customSchema),
+      insertImage,
+    ],
   });
   editor.onEditorContentChange(() => {
-    // Get and log all top-level, i.e. non-nested blocks in the editor.
-    const blocks = editor.topLevelBlocks;
-    console.table(blocks);
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(async () => {
+      console.log('saving...');
+      const blocks = editor.topLevelBlocks;
+      await fetchAPIRequest(`/task/edit/content?taskId=${taskId}`, 'PUT', {
+        blocks,
+      });
+    }, 5000);
+
+    // console.table(blocks);
   });
 
   // Renders the editor instance using a React component.
