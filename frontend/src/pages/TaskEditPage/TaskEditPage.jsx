3a0f837a8f103c306a0b8c992ff77cd415ff37ce
Alex Xu <xulex6@gmail.com>
2023-09-30 16:00:19 +1000

    feat: add side panel as flex element rather than fixed

--- frontend/src/pages/TaskEditPage/TaskEditPage.jsx
+++ frontend/src/pages/TaskEditPage/TaskEditPage.jsx
@@ -1,22 +1,25 @@
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { fetchAPIRequest } from '../../helpers';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { LoadingScreen, Editor } from '../../components';
+import SidePanel from './SidePanel';
 import TaskActionBtnGroup from './TaskActionBtnGroup';
+import TaskProvider from '../../context/TaskSidePanelContext';
 
 const TaskEditPage = () => {
   const { projectId } = useParams();
   const [isLoading, setIsLoading] = useState(true);
-  const [taskList, setTaskList] = useState([]);
+  const [taskContentList, setTaskContentList] = useState([]);
+
   useEffect(() => {
     const getDashboard = async () => {
       setIsLoading(true);
-      const taskData = await fetchAPIRequest(
+      const taskContent = await fetchAPIRequest(
         `/task/get/content?projectId=${projectId}`,
         'GET'
       );
-      setTaskList(taskData);
+      setTaskContentList(taskContent);
       setTimeout(() => {
         setIsLoading(false);
       }, 500);
@@ -28,40 +31,46 @@ const TaskEditPage = () => {
   return isLoading ? (
     <LoadingScreen />
   ) : (
-    <Box
-      sx={{
-        py: 4,
-        px: 4,
-        boxSizing: 'border-box',
-        height: 'fit-content',
-      }}
-    >
-      <Box
-        sx={{
-          textAlign: 'left',
-          py: 2,
-          mb: 2,
-        }}
-      >
-        <Typography
-          variant="h1"
-          sx={{ fontSize: '30px', fontWeight: 600, mb: 3 }}
+    <TaskProvider>
+      <Box sx={{ display: 'flex' }}>
+        <Box
+          sx={{
+            py: 4,
+            px: 4,
+            boxSizing: 'border-box',
+            height: 'fit-content',
+            width: '100%',
+          }}
         >
-          Task Edit
-        </Typography>
-      </Box>
-      {taskList.map((task, index) => (
-        <Box key={index} sx={{ mt: 5 }}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography sx={{ fontSize: '24px', fontWeight: 600, mb: 3 }}>
-              {task.name}
+          <Box
+            sx={{
+              textAlign: 'left',
+              py: 2,
+              mb: 2,
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{ fontSize: '30px', fontWeight: 600, mb: 3 }}
+            >
+              Task Edit
             </Typography>
-            <TaskActionBtnGroup />
           </Box>
-          <Editor initialBlocks={task.blocks} taskId={task.id} />
+          {taskContentList.map((task, index) => (
+            <Box key={index} sx={{ mt: 5 }}>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography sx={{ fontSize: '24px', fontWeight: 600, mb: 3 }}>
+                  {task.name}
+                </Typography>
+                <TaskActionBtnGroup taskId={task.id} />
+              </Box>
+              <Editor initialBlocks={task.blocks} taskId={task.id} />
+            </Box>
+          ))}
         </Box>
-      ))}
-    </Box>
+        <SidePanel />
+      </Box>
+    </TaskProvider>
   );
 };
 export default TaskEditPage;
---
+++ frontend/src/pages/TaskPage/index.js
@@ -0,0 +1,1 @@
+export { default as TaskPriority } from './TaskPriority.jsx';
